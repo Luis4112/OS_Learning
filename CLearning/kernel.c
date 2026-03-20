40953b08ed7f0238fc98cdeda0f55b6e321ca9d0
Luis Baião <luisfbaiao@hotmail.com>
2026-03-20 09:37:54 +0000

    added kernel.c and linker script

--- CLearning/kernel.c
+++ CLearning/kernel.c
@@ -1,10 +1,26 @@
-ENTRY(kernel_main)
-     SECTIONS {
-	. = 0x10000;
+typedef unsigned short uint16_t;
 
-	.text : { *(.text)}
-	
-	.data : { *(.data)}
+/* to write : a function that loops through __bss_start and  end as
+ * start and stopping point, walking through memory and zeroing it out.*/
+
+extern char __bss_start;
+extern char __bss_end;
+
+
+void  bsszero(){
+  
+     char *S = &__bss_start;
+     for (; S < &__bss_end; S++)
+        *S = 0;
+
+}
+
+void kernel_main(){
+    bsszero();
+    uint16_t* vga_buffer = (uint16_t*) 0xB8000;
+    vga_buffer[0] = (0x07 << 8) | 'H';   
+
+    vga_buffer[1] = (0x07 << 8) | 'e';
     
-    .bss_start = .;
+
 }
